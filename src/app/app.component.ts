4a717e0648465ce65c343308096eafbbe3c5dc78
beetlezhuk <azhuk.personal@gmail.com>
2019-12-20 00:19:09 +0100

    Split admin and user logic (#5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eShopperUI';
+  path: String = "";
+
+  constructor(router: Router) {
+    router.events.subscribe((val) => {
+      this.path = router.url;
+      console.log(">>>>>>>>>>>>>>>" + this.path)
+    })
+  }
+
+  isAdmin(): boolean {
+    return this.path.includes("/admin");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,27 @@ import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { HomeComponent } from './components/home/home.component';
-import { CustomernavComponent } from './components/customernav/customernav.component';
-import { BannersComponent } from './components/banners/banners.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { CustomernavComponent } from './components/user/customernav/customernav.component';
+import { BannersComponent } from './components/user/banners/banners.component';
+import { NavigationComponent } from './components/user/navigation/navigation.component';
 import { HomeModule } from './modules/home/home.module';
-import { ProductsComponent } from './components/products/products.component';
+import { ProductsComponent } from './components/user/products/products.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/user/home/home.component';
 import { ProductsModule } from './modules/products/products.module';
 import { AdminComponent } from './components/admin/admin.component';
 import { ItemsComponent } from './components/admin/items/items.component';
 import { CategoriesComponent } from './components/admin/categories/categories.component';
 import { CustomersComponent } from './components/admin/customers/customers.component';
 import { OrdersComponent } from './components/admin/orders/orders.component';
+import { UserComponent } from './components/user/user.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'products', component: ProductsComponent }
+  { path: 'products', component: ProductsComponent },
+  { path: 'admin', component: ItemsComponent },
+  { path: 'admin/items', component: ItemsComponent },
+  { path: 'admin/orders', component: OrdersComponent }
 ];
 
 @NgModule({
@@ -35,7 +39,8 @@ const appRoutes: Routes = [
     ItemsComponent,
     CategoriesComponent,
     CustomersComponent,
-    OrdersComponent
+    OrdersComponent,
+    UserComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
