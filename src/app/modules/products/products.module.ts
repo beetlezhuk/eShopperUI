e91edb1a33ff9db3762c95df63c9ae5133e3bf81
beetlezhuk <azhuk.personal@gmail.com>
2019-12-20 00:16:32 +0100

    Split admin and user logic

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from 'src/app/components/products/products.component';
+import { ProductsComponent } from 'src/app/components/user/products/products.component';
 
 @NgModule({
   declarations: [ProductsComponent],
